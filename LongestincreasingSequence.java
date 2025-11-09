3b77ee91c7112a7941e6375b70e10c09e7d4c766
daulenskript <kazbekok7@gmail.com>
2025-11-09 19:42:38 +0600

    merhaba

---
+++ LongestincreasingSequence.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int [] Lis = new int[nums.length];
+        Arrays.fill(Lis , 1);
+        int max = 1;
+
+        for(int i = 1 ; i < nums.length ; i++){
+            for(int j = 0 ; j < i ;j++){
+                if(nums[i] > nums[j]){
+                    Lis[i] = Math.max(Lis[i] , 1 + Lis[j]);
+                    max = Math.max(Lis[i] , max);
+                }
+            }
+        }
+        return max;
+    }
+}
